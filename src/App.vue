88c8cf72e6dd47c57bf37127bd1954343ca98090
Scott Landes <scottlandes@gmail.com>
2018-10-23 09:08:27 -0700

    Finished main note functionality

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,116 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Note</span>
-        <span class="font-weight-light">xplosion</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click="modal = true">
-          <v-icon>add</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <v-app>
+        <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+            <span>Note</span>
+            <span class="font-weight-light">xplosion</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+            <v-btn icon @click="modal = true">
+                <v-icon>add</v-icon>
+            </v-btn>
+        </v-toolbar>
 
-    <v-content>
-      <v-container fluid grid-list-md>
-        <v-layout row wrap>
-          <Note v-for="note in notes" 
-                v-bind:key="note.id" 
-                :title="note.title" 
-                :note="note.note" 
-                :id="note.id">
-          </Note>
-          <v-dialog
-            v-model="modal"
-            max-width="500px"
-            persistent
-          >
+        <v-content>
+            <v-container fluid grid-list-md>
+                <v-layout row wrap>
+                    <Note v-for="(note, index) in notes" 
+                          v-bind:key="index" 
+                          v-bind:note="note"
+                          v-on:remove-note="removeNote">
+                    </Note>
+                </v-layout>
+            </v-container>      
+        </v-content>
+        <v-dialog v-model="modal"
+                  max-width="500px"
+                  persistent>
             <v-card>
-              <v-card-title class="headline">Add a new note</v-card-title>
-      
-              <v-form ref="form">
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12>
-                        <v-text-field
-                          label="Title" 
-                          v-model="input.title"
-                          required
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-textarea
-                          name="input-7-1"
-                          label="Note"
-                          v-model="input.note"
-                          hint="Enter your note here!"
-                        ></v-textarea>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-card-text>
-        
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="green darken-1"
-                    flat="flat"
-                    @click="modal = false"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    color="green darken-1"
-                    flat="flat"
-                    @click="submit"
-                  >
-                    Agree
-                  </v-btn>
-                </v-card-actions>
-              </v-form>
+                <v-card-title class="headline">Add a new note</v-card-title>
+
+                <v-form ref="form">
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex xs12>
+                                    <v-text-field label="Title" 
+                                                v-model="input.title"
+                                                required>
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex xs12>
+                                    <v-textarea name="input-7-1"
+                                                label="Note"
+                                                v-model="input.note"
+                                                hint="Enter your note here!">
+                                    </v-textarea>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+            
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1"
+                               flat="flat"
+                               @click="modal = false">
+                            Cancel
+                        </v-btn>
+                        <v-btn color="green darken-1"
+                               flat="flat"
+                               @click="submit">
+                            Create Note
+                        </v-btn>
+                    </v-card-actions>
+                </v-form>
             </v-card>
-          </v-dialog>
-        </v-layout>
-      </v-container>      
-    </v-content>
-  </v-app>
+        </v-dialog>
+    </v-app>
 </template>
 
 <script>
 import Note from './components/Note'
 
 export default {
-  name: 'App',
-  components: {
-    Note
-  },
-  data () {
-    return {
-      modal: false,
-      input: {
-        note: '',
-        title: ''
-      },
-      notes: []
-    }
-  },
+    name: 'App',
 
-  methods: {
-    submit() {
-      this.notes.push({
-        title: this.input.title,
-        note: this.input.note
-      })
-
-      this.clear()
-      this.modal = false
+    components: {
+        Note
     },
 
-    clear() {
-      this.input.title = ''
-      this.input.note = ''
+    data () {
+        return {
+            modal: false,
+            notes: [],
+            input: {
+                note: '',
+                title: '',
+            },
+        }
+    },
+
+    methods: {
+        submit() {
+            this.notes.push({
+                title: this.input.title,
+                note: this.input.note
+            })
+
+            this.clear()
+            this.modal = false
+        },
+
+        clear() {
+            this.input.title = ''
+            this.input.note = ''
+            this.input.id = null
+        },
+
+        removeNote(note) {
+            const noteIndex = this.notes.indexOf(note);
+
+            this.notes.splice(noteIndex, 1);
+        }
     }
-  }
 }
 </script>
--- src/components/Note.vue
+++ src/components/Note.vue
@@ -2,16 +2,50 @@
     <v-flex xs12 sm8 md4>
         <v-card color="white" class="ma-3">
             <v-responsive :aspect-ratio="16/9">
-                <v-toolbar color="blue" dark>
-                    <v-toolbar-title>{{ title }}</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn icon>
-                        <v-icon>close</v-icon>
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text color="blue">
-                    {{ note }}
-                </v-card-text>
+                <div v-if="isEditing">
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <v-layout wrap>
+                                <v-flex xs12>
+                                    <v-text-field label="Title" 
+                                                v-model="note.title"
+                                                required>
+                                    </v-text-field>
+                                </v-flex>
+                                <v-flex xs12>
+                                    <v-textarea name="input-7-1"
+                                                label="Note"
+                                                v-model="note.note"
+                                                hint="Enter your note here!">
+                                    </v-textarea>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1"
+                               flat="flat"
+                               @click="isEditing = false">
+                            Close
+                        </v-btn>
+                    </v-card-actions>
+                </div>
+                <div v-else>
+                    <v-toolbar color="blue" dark>
+                        <v-toolbar-title>{{ note.title }}</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon  @click="isEditing = true">
+                            <v-icon>edit</v-icon>
+                        </v-btn>
+                        <v-btn icon @click="removeNote(note)">
+                            <v-icon>close</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                    <v-card-text color="blue">
+                        {{ note.note }}
+                    </v-card-text>
+                </div>
             </v-responsive>
         </v-card>
     </v-flex>
@@ -20,10 +54,20 @@
 <script>
 export default {
     props: [
-        'id',
-        'title',
         'note'
-    ]
+    ],
+
+    data() {
+        return {
+            isEditing: false,
+        };
+    },
+
+    methods: {
+        removeNote(note) {
+            this.$emit('remove-note', note);
+        }
+    }
 }
 </script>
 
