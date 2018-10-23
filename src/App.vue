d76c09a5022d29aadd529501cdfdd4e3ccd15fea
Scott Landes <scottlandes@gmail.com>
2018-10-22 22:20:25 -0700

    Notes!  Notes! Yay!

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,114 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Note</span>
+        <span class="font-weight-light">xplosion</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn icon @click="modal = true">
+          <v-icon>add</v-icon>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <Note v-for="note in notes" 
+                v-bind:key="note.id" 
+                :title="note.title" 
+                :note="note.note" 
+                :id="note.id">
+          </Note>
+          <v-dialog
+            v-model="modal"
+            max-width="500px"
+            persistent
+          >
+            <v-card>
+              <v-card-title class="headline">Add a new note</v-card-title>
+      
+              <v-form ref="form">
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12>
+                        <v-text-field
+                          label="Title" 
+                          v-model="input.title"
+                          required
+                        ></v-text-field>
+                      </v-flex>
+                      <v-flex xs12>
+                        <v-textarea
+                          name="input-7-1"
+                          label="Note"
+                          v-model="input.note"
+                          hint="Enter your note here!"
+                        ></v-textarea>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-text>
+        
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="green darken-1"
+                    flat="flat"
+                    @click="modal = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    color="green darken-1"
+                    flat="flat"
+                    @click="submit"
+                  >
+                    Agree
+                  </v-btn>
+                </v-card-actions>
+              </v-form>
+            </v-card>
+          </v-dialog>
+        </v-layout>
+      </v-container>      
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Note from './components/Note'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Note
   },
   data () {
     return {
-      //
+      modal: false,
+      input: {
+        note: '',
+        title: ''
+      },
+      notes: []
+    }
+  },
+
+  methods: {
+    submit() {
+      this.notes.push({
+        title: this.input.title,
+        note: this.input.note
+      })
+
+      this.clear()
+      this.modal = false
+    },
+
+    clear() {
+      this.input.title = ''
+      this.input.note = ''
     }
   }
 }
---
+++ src/components/Note.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-flex xs12 sm8 md4>
+        <v-card color="white" class="ma-3">
+            <v-responsive :aspect-ratio="16/9">
+                <v-toolbar color="blue" dark>
+                    <v-toolbar-title>{{ title }}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                </v-toolbar>
+                <v-card-text color="blue">
+                    {{ note }}
+                </v-card-text>
+            </v-responsive>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+export default {
+    props: [
+        'id',
+        'title',
+        'note'
+    ]
+}
+</script>
+
+<style>
+
+</style>
